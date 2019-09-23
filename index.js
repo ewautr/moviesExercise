450aecf0b0542a7c3702d2e8d543cf65c81fe241
Ewa Utracka <ewczi@Ewas-MacBook-Pro.local>
2019-09-23 12:32:35 +0200

    get and post added

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 "use strict";
 
+document.querySelector("button").addEventListener("click", e => {
+  post();
+});
+
+get();
+
 function get() {
   fetch("https://movies-d95a.restdb.io/rest/movies", {
     method: "get",
@@ -11,16 +17,41 @@ function get() {
   })
     .then(res => res.json())
     .then(movies => {
-      movies.forEach(movie => {
-        const template = document.querySelector("template").content;
-        const clone = template.cloneNode(true);
-        clone.querySelector("h1").textContent = movie.title;
-        clone.querySelector("h2").textContent = movie.director;
-        clone.querySelector("p").textContent = movie.actors;
-        clone.querySelector("aside").textContent = movie.releaseyear;
-        document.querySelector("#app").appendChild(clone);
-      });
+      movies.forEach(addMovieToDom);
     });
 }
 
-get();
+function post() {
+  const newData = {
+    title: "Pulp Fiction",
+    director: "Tarantino",
+    actors: "Uma Thurman, John Travolta",
+    releaseyear: "1999"
+  };
+  const postData = JSON.stringify(newData);
+
+  fetch(`https://movies-d95a.restdb.io/rest/movies`, {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+      "x-apikey": "5d887e76fd86cb75861e2628",
+      "cache-control": "no-cache"
+    },
+    body: postData
+  })
+    .then(res => res.json())
+    .then(data => {
+      window.location = "";
+      addMovieToDom(data);
+    });
+}
+
+function addMovieToDom(movie) {
+  const template = document.querySelector("template").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector("h1").textContent = movie.title;
+  clone.querySelector("h2").textContent = movie.director;
+  clone.querySelector("p").textContent = movie.actors;
+  clone.querySelector("aside").textContent = movie.releaseyear;
+  document.querySelector("#app").appendChild(clone);
+}
